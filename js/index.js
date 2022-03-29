4f510864ca7e26075bb3450c710660c56ce8a52c
EternalTempter <FrOsTiK12345678998@gmail.com>
2022-03-29 20:55:34 +0200

    asdfdasf

--- js/index.js
+++ js/index.js
@@ -1,24 +1,47 @@
 let serverData;
-document.querySelector('.catalog').addEventListener('click',function(){
+document.querySelector('.cartierCategoryButton').addEventListener('click',function(){
+    showDataOnPage('cartier',document.querySelector('.cartierCategoryButton'));
+});
+document.querySelector('.tissotCategoryButton').addEventListener('click',function(){
+    showDataOnPage('tissot',document.querySelector('.tissotCategoryButton'));
+});
+document.querySelector('.rolexCategoryButton').addEventListener('click',function(){
+    showDataOnPage('rolex',document.querySelector('.rolexCategoryButton'));
+});
+document.querySelector('.omegaCategoryButton').addEventListener('click',function(){
+    showDataOnPage('omega',document.querySelector('.omegaCategoryButton'));
+});
+document.querySelector('.breguetCategoryButton').addEventListener('click',function(){
+    showDataOnPage('breguet',document.querySelector('.breguetCategoryButton'));
+});
+
+function showDataOnPage(watchesModel,buttonToTurnOn){
+    getData();
+    setTimeout(() => buildCards(serverData,watchesModel),200);
+    document.querySelectorAll('.categoryButton').forEach(button => {
+        button.classList.remove('on');
+    });
+    buttonToTurnOn.classList.add('on');
+}
+function getData(){
     fetch('data.json')
         .then(response => response.json())
         .then(json => setData(json));
-    
-        setTimeout(() => buildCards(serverData),100);
-});
-    
+}
 function setData(data){
     serverData = data;
 }
     
-function buildCards(data){
+function buildCards(data,watchesModel){
     let content = document.querySelector('.content');
-        Array.from(Object.keys(data.catalog.rolex)).forEach(watchesName => {
-            content.innerHTML += `<div class="card">
-                <img src="${data.catalog.rolex[watchesName].imageUrl}">
+    content.innerHTML = '';
+        Array.from(Object.keys(data.catalog[watchesModel])).forEach(watchesName => {
+            content.innerHTML += 
+            `<div class="card">
+                <img src="${data.catalog[watchesModel][watchesName].imageUrl}">
                 <h3>${watchesName}</h3>
-                <p>${data.catalog.rolex[watchesName].desc}</p>
-                <span>${data.catalog.rolex[watchesName].price}</span>
+                <p>${data.catalog[watchesModel][watchesName].desc}</p>
+                <span>${data.catalog[watchesModel][watchesName].price}</span>
             </div>`;
         });
 }
