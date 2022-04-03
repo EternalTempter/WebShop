15cc96885b4bc7a0d378b079aa9db53cfa9465fa
EternalTempter <FrOsTiK12345678998@gmail.com>
2022-04-04 00:14:45 +0200

    asdfasdfasdfsaf

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -41,17 +41,45 @@ window.addEventListener('DOMContentLoaded',function(){
                     <div class="shopCartInfoHolder">
                         <h3>${watchesName}</h3>
                         <p>${allServerData.catalog.rolex[watchesName].desc}</p>
-                        <span>${allServerData.catalog.rolex[watchesName].price}</span>
+                        <span id="watchesPrice">${allServerData.catalog.rolex[watchesName].price}</span>
                         <div class="shopCartAmount">
                             <p>Количество</p>
-                            <img src="images/minus.png">
+                            <img src="images/minus.png" class="setMinus">
                             <span>1</span>
-                            <img src="images/plus.png">
+                            <img src="images/plus.png" class="setPlus">
                         </div>
                     </div>
                 </div>`;
+
                 }
             });
-        
+        content.innerHTML += '<div class="miniShopCartItemLine"></div>';
+        content.innerHTML += `<p class="totalPriceWrap">Итого: <span class="totalPrice">${getSumOfAllWatches()} €</span></p>`;
+        content.innerHTML += '<div class="orderButton">Заказать</div>';
+        document.querySelectorAll('.setMinus').forEach(minus => {
+            minus.addEventListener('click',function(){
+                minus.nextElementSibling.textContent = 
+                        (Number(minus.nextElementSibling.textContent) > 0) 
+                        ? (Number(minus.nextElementSibling.textContent) - 1) 
+                        : Number(minus.nextElementSibling.textContent);
+                document.querySelector('.totalPrice').textContent = `${getSumOfAllWatches()} €`;
+            });
+        });
+        document.querySelectorAll('.setPlus').forEach(plus => {
+            plus.addEventListener('click',function(){
+                plus.previousElementSibling.textContent = 
+                (Number(plus.previousElementSibling.textContent) < 10) 
+                ? (Number(plus.previousElementSibling.textContent) + 1) 
+                : Number(plus.previousElementSibling.textContent);
+                document.querySelector('.totalPrice').textContent = `${getSumOfAllWatches()} €`;
+            });
+        });
+    }
+    function getSumOfAllWatches(){
+        let total = 0;
+        document.querySelectorAll('#watchesPrice').forEach(watchesPrice => {
+            total += ((Number(watchesPrice.textContent.replace(' €','').replace(' ',''))) * (Number(watchesPrice.nextElementSibling.childNodes[5].textContent))); 
+        });
+        return total;
     }
 });
