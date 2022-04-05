44a1e2efffdeafb860479c754d0477b8c0e5f21b
NEkaterina <nagibina_katya@inbox.ru>
2022-04-05 17:37:53 +0200

    asdfasdfasdfsadfsadfsa

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -2,17 +2,21 @@ let menuBtn = document.querySelector('.menuButton');
 let burgerMenu = document.querySelector('.burgerMenu');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuBtn.classList.add('open');
-    burgerMenu.classList.add('openedBurgerMenu');
+    burgerMenu.classList.add('opened');
     document.querySelector('.mainLabel').classList.toggle('off');
     document.querySelector('.watchCollectionButton').classList.toggle('off');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
-    burgerMenu.classList.remove('openedBurgerMenu');
+    burgerMenu.classList.remove("opened")
+    burgerMenu.classList.add("hide")
+    setTimeout(() => {
+      burgerMenu.classList.remove("hide")
+    }, 800)
+    menuOpen = false
     document.querySelector('.mainLabel').classList.toggle('off');
     document.querySelector('.watchCollectionButton').classList.toggle('off');
-    menuOpen = false;
   }
 });
